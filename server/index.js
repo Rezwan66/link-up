92fc00da7c5183e782bbb854a15955ffc333364c
Rezwan66 <shaikhrezwan66@gmail.com>
2024-12-27 21:15:32 +0100

    added the posts route to the server with get all posts and create a post APIs

--- server/index.js
+++ server/index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
-// require('dotenv').config();
+app.use(express.json());
+require('dotenv').config();
 // const cors = require('cors');
 const port = process.env.PORT || 3001;
 const db = require('./models');
+
+// Routers
+const postRouter = require("./routes/Posts");
+app.use("/posts", postRouter);
 
 db.sequelize.sync().then(() => {
     app.listen(port, () => {
         console.log(`LinkUp Server is running on port ${port}`);
     });
-})
-
-
+});
