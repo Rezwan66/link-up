f177b518c9e199297bae814ed7f81b585be560f9
Rezwan66 <shaikhrezwan66@gmail.com>
2024-12-31 00:12:25 +0100

    created the formik form to take input for adding a post

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
-function CreatePost() {
+const CreatePost = () => {
+    const initialValues = {
+        title: '',
+        postText: '',
+        username: ''
+    };
+    const onSubmit = (data) => {
+        console.log(data);
+    };
+    const validationSchema = Yup.object().shape({
+        title: Yup.string().required('You must add a title!'),
+        postText: Yup.string().required(),
+        username: Yup.string().min(3).max(15).required()
+    });
     return (
-        <div>CreatePost</div>
-    );
-}
+        <div className="createPostPage">
+            <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
+                <Form className='formContainer'>
+                    <label>Title:</label>
+                    <ErrorMessage name='title' component='span' />
+                    <Field id='inputCreatePost' name='title' placeholder='Eg. Title...' />
+                    <label>Post:</label>
+                    <ErrorMessage name='postText' component='span' />
+                    <Field id='inputCreatePost' name='postText' placeholder='Eg. Post...' />
+                    <label>Username:</label>
+                    <ErrorMessage name='username' component='span' />
+                    <Field id='inputCreatePost' name='username' placeholder='Eg. John123...' />
 
+                    <button type='submit'>Create Post</button>
+                </Form>
+            </Formik>
+        </div>
+    );
+};
 export default CreatePost;
