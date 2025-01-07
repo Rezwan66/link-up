ab8f74719f4bd0812b8898e8db3c163cbce92697
Rezwan66 <shaikhrezwan66@gmail.com>
2025-01-07 22:46:25 +0100

    added comments input and loaded comments

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -4,42 +4,53 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 
 const CreatePost = () => {
-    const navigate = useNavigate();
-    const initialValues = {
-        title: '',
-        postText: '',
-        username: ''
-    };
-    const onSubmit = (data) => {
-        axios.post('http://localhost:3001/posts', data).then(res => {
-            if (res.statusText === 'OK' || res.status === 200) {
-                navigate('/');
-            }
-        });
-    };
-    const validationSchema = Yup.object().shape({
-        title: Yup.string().required('You must add a title!'),
-        postText: Yup.string().required(),
-        username: Yup.string().min(3).max(15).required()
+  const navigate = useNavigate();
+  const initialValues = {
+    title: '',
+    postText: '',
+    username: '',
+  };
+  const onSubmit = data => {
+    axios.post('http://localhost:3001/posts', data).then(res => {
+      if (res.statusText === 'OK' || res.status === 200) {
+        navigate('/');
+      }
     });
-    return (
-        <div className="createPostPage">
-            <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
-                <Form className='formContainer'>
-                    <label>Title:</label>
-                    <ErrorMessage name='title' component='span' />
-                    <Field id='inputCreatePost' name='title' placeholder='Eg. Title...' />
-                    <label>Post:</label>
-                    <ErrorMessage name='postText' component='span' />
-                    <Field id='inputCreatePost' name='postText' placeholder='Eg. Post...' />
-                    <label>Username:</label>
-                    <ErrorMessage name='username' component='span' />
-                    <Field id='inputCreatePost' name='username' placeholder='Eg. John123...' />
-
-                    <button type='submit'>Create Post</button>
-                </Form>
-            </Formik>
-        </div>
-    );
+  };
+  const validationSchema = Yup.object().shape({
+    title: Yup.string().required('You must add a title!'),
+    postText: Yup.string().required(),
+    username: Yup.string().min(3).max(15).required(),
+  });
+  return (
+    <div className="createPostPage">
+      <Formik
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+      >
+        <Form className="formContainer">
+          <label>Title:</label>
+          <ErrorMessage name="title" component="span" />
+          <Field id="inputCreatePost" name="title" placeholder="Eg. Title..." />
+          <label>Post:</label>
+          <ErrorMessage name="postText" component="span" />
+          <Field
+            id="inputCreatePost"
+            name="postText"
+            placeholder="Eg. Post..."
+          />
+          <label>Username:</label>
+          <ErrorMessage name="username" component="span" />
+          <Field
+            id="inputCreatePost"
+            name="username"
+            placeholder="Eg. John123..."
+          />
+          <button type="submit">Create Post</button>
+        </Form>
+      </Formik>
+    </div>
+  );
 };
 export default CreatePost;
--- client/src/pages/Post.js
+++ client/src/pages/Post.js
@@ -1,19 +1,24 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 const Post = () => {
     const { id } = useParams();
     const [postObj, setPostObj] = useState({});
+    const [comments, setComments] = useState([]);
 
     useEffect(() => {
         axios.get(`http://localhost:3001/posts/byId/${id}`).then(res => {
             setPostObj(res?.data);
         });
+        axios.get(`http://localhost:3001/comments/${id}`).then(res => {
+            setComments(res?.data);
+        });
     }, [id]);
     return (
         <>
             <div className="postPage">
+                {/* Post */}
                 <div className="leftSide">
                     <div className="post" id="individual">
                         <div className="title">{postObj?.title}</div>
@@ -21,7 +26,20 @@ const Post = () => {
                         <div className="footer">{postObj?.username}</div>
                     </div>
                 </div>
-                <div className="rightSide">Comment Section</div>
+                {/* Comment Section */}
+                <div className="rightSide">
+                    <div className="addCommentContainer">
+                        <input type="text" name="" placeholder="Comment..." id="" />
+                        <button>Add Comment</button>
+                    </div>
+                    <div className="listOfComments">
+                        {comments?.map((comment, idx) => (
+                            <div key={idx} className="comment">
+                                {comment?.commentBody}
+                            </div>
+                        ))}
+                    </div>
+                </div>
             </div>
         </>
     );
