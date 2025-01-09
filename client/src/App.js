93f70b944a4a0f872a7d8a4e82b39a9b026c45e6
Rezwan66 <shaikhrezwan66@gmail.com>
2025-01-09 23:29:05 +0100

    created login and reguister routes in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,23 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import CreatePost from './pages/CreatePost';
 import Post from './pages/Post';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <div className='navbar'>
+        <div className="navbar">
           <Link to="/">HomePage</Link>
           <Link to="createpost">Create A Post</Link>
         </div>
         <Routes>
-          <Route path='/' exact element={<Home />} />
-          <Route path='/createpost' exact element={<CreatePost />} />
-          <Route path='/post/:id' exact element={<Post />} />
+          <Route path="/" exact element={<Home />} />
+          <Route path="/createpost" exact element={<CreatePost />} />
+          <Route path="/post/:id" exact element={<Post />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/register" exact element={<Register />} />
         </Routes>
       </Router>
     </div>
